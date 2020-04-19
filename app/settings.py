2a6cdccab76bb6343fc21108a381429765b40724
CHOUTHRI <naveenrajesh2222@gmail.com>
2020-04-19 12:57:28 +0530

    sucess

--- app/settings.py
+++ app/settings.py
@@ -125,14 +125,12 @@ STATICFILES_DIRS = [
 PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
 STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Extra lookup directories for collectstatic to find static files
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )
 
 #  Add configuration for static files storage using whitenoise
-STATICFILES_STORAGE = 'whitenoise.storage.GzipManifestStaticFilesStorage'
-
 
 ALLOWED_HOSTS = ['127.0.0.1','.herokuapp.com']
